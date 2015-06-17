d4b2120e70d91e6f309493d23254dc422bc33c98
maria <mar_ortiz@misena.edu.co>
2015-06-17 11:20:36 -0500

    creacion de tablas y correccion de errores

--- install.php
+++ install.php
@@ -11,27 +11,73 @@
 		    /* Creacion de la tabla Usuarios */
 		    $query = "CREATE TABLE 'Usuarios' (
 						'idUsuario'	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-						'Nombres'	TEXT NOT NULL,
-						'Apellidos'	TEXT NOT NULL,
-						'Direccion'	TEXT NOT NULL,
-						'Telefono'	TEXT NOT NULL,
-						'Estado'	TEXT NOT NULL DEFAULT 'Activo'
+						'usuario'	TEXT NOT NULL,
+						'contrasena'	TEXT NOT NULL,
+						'nombre'	TEXT NOT NULL,
+						'apellidouno'	TEXT NOT NULL,
+						'apellidodos'	TEXT NOT NULL,
+						'titulo'	TEXT NOT NULL,
+						'descripcion'	TEXT NOT NULL,
+						'foto'	TEXT NOT NULL,
+						'email'	TEXT NOT NULL,
+						'permisos'	INTEGER NOT NULL
+						
 					);"; //Creacion del query para crear la tabla.
 		    $result = $db->exec($query); //Ejecutamos el query. Se usa exec para todos los casos excepto para los select.
 		    echo ($result === false) ? "<i class='fa fa-times-circle'></i> No se pudo crear la Tabla Usuarios."."<br/>" : "<i class='fa fa-check-square-o'></i> Se creo correctamente la Tabla Usuarios."."<br/>";
 
-		    /* Creacion de la tabla Automoviles */
-		    $query = "CREATE TABLE `Automovil` (
-						`idAutomovil`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-						`Marca`	TEXT NOT NULL,
-						`Modelo`	TEXT NOT NULL,
-						`Color`	TEXT NOT NULL,
-						`Placa`	TEXT NOT NULL,
-						`Estado`	TEXT NOT NULL
-					);";
+		    /* Creacion de la tabla configusuarios */
+		    $query = "CREATE TABLE `configusuarios` (
+						`idconfig`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+						`usuario`	TEXT NOT NULL,
+						`piel`	TEXT NOT NULL,
+						`respuestas` TEXT NOT NULL
+						);";
 			$result = $db->exec($query); //Ejecutamos el query. Se usa exec para todos los casos excepto para los select.
-			echo ($result === false) ? "<i class='fa fa-times-circle'></i> No se pudo crear la Tabla Automovil."."<br/>" : "<i class='fa fa-check-square-o'></i> Se creo correctamente la Tabla Automovil."."<br/>";
+			echo ($result === false) ? "<i class='fa fa-times-circle'></i> No se pudo crear la Tabla configusuarios."."<br/>" : "<i class='fa fa-check-square-o'></i> Se creo correctamente la Tabla configusuarios."."<br/>";
+              
+              /* Creacion de la tabla post */
+		    $query = "CREATE TABLE 'post' (
+						'idpost'	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+						'utc'	INTEGER NOT NULL,
+						'anio'	INTEGER NOT NULL,
+						'mes'	INTEGER NOT NULL,
+						'hora'	INTEGER NOT NULL,
+						'minuto'	INTEGER NOT NULL,
+						'segundo'	INTEGER NOT NULL,
+						'usuario'	TEXT NOT NULL,
+						'titulo'	TEXT NOT NULL,
+						'subtitulo'	TEXT NOT NULL,
+						'email'	TEXT NOT NULL,
+						'icono'	TEXT NOT NULL,
+						'texto'	TEXT NOT NULL,
+						'imagen'	TEXT NOT NULL,
+						'video'	TEXT NOT NULL,
+                        'sonido'	TEXT NOT NULL
+						
+					);"; //Creacion del query para crear la tabla.
+		    $result = $db->exec($query); //Ejecutamos el query. Se usa exec para todos los casos excepto para los select.
+		    echo ($result === false) ? "<i class='fa fa-times-circle'></i> No se pudo crear la Tabla post."."<br/>" : "<i class='fa fa-check-square-o'></i> Se creo correctamente la Tabla post."."<br/>";
 
+           
+           /* Creacion de la tabla logs */
+		    $query = "CREATE TABLE logs (
+						'idlogs'	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+						'utc'	INTEGER NOT NULL,
+						'anio'	INTEGER NOT NULL,
+						'mes'	INTEGER NOT NULL,
+						'dia'	INTEGER NOT NULL,
+						'hora'	INTEGER NOT NULL,
+						'minuto'	INTEGER NOT NULL,
+						'segundo'	INTEGER NOT NULL,
+						'ip'	TEXT NOT NULL,
+						'navegador'	TEXT NOT NULL,
+						'usuario'	TEXT NOT NULL,
+						'operacion'	TEXT NOT NULL
+					);"; //Creacion del query para crear la tabla.
+		    $result = $db->exec($query); //Ejecutamos el query. Se usa exec para todos los casos excepto para los select.
+		    echo ($result === false) ? "<i class='fa fa-times-circle'></i> No se pudo crear la Tabla logs."."<br/>" : "<i class='fa fa-check-square-o'></i> Se creo correctamente la Tabla logs."."<br/>";
+        
 		    $db = NULL; //Cerramos la conexion a la Base de datos.
 		}catch(PDOException $e){
 		    echo $e->getMessage();
